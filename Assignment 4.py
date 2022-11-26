5e1bfa7f18a656b73d76f30a8efd2d85a6a54adf
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-11-26 17:39:45 +0200

    Update Assignment 4.py

--- Assignment 4.py
+++ Assignment 4.py
@@ -20,7 +20,7 @@ class SinglyLinkedList:
             self.__insertAtHead(newNode)
             return
 
-        traverse = self.__traverseList(index + 1)   # stop at the given index
+        traverse = self.traverseList(index)   # stop at the given index
         newNode.next = traverse
         self.prevNode.next = newNode
 
@@ -47,7 +47,7 @@ class SinglyLinkedList:
             self.__removeAtHead()
             return
 
-        traverse = self.__traverseList(index + 1)   # stop at the given index
+        traverse = self.traverseList(index + 1)   # stop at the given index
         self.prevNode.next = traverse.next
         traverse.next = None
         del traverse
@@ -68,11 +68,11 @@ class SinglyLinkedList:
             temp = self.head
 
     def get(self, index: int):
-        traverse = self.__traverseList(index)
+        traverse = self.traverseList(index)
         return traverse.data
 
     def set(self, data, index: int) -> None:
-        traverse = self.__traverseList(index)
+        traverse = self.traverseList(index)
         traverse.data = data
 
     def contains(self, element) -> bool:
@@ -82,9 +82,9 @@ class SinglyLinkedList:
                 return True
             traverse = traverse.next
 
-    def __traverseList(self, index):   # will be used any time we need to traverse the list till a given index
+    def traverseList(self, index):   # will be used any time we need to traverse the list till a given index
         traverse, self.prevNode = (self.head,) * 2
-        for _ in range(index - 1):
+        for _ in range(index):
             self.prevNode = traverse
             traverse = traverse.next
         return traverse
@@ -96,7 +96,7 @@ class SinglyLinkedList:
             currentNode = currentNode.next
 
     def printSubList(self, fromIdx: int, toIdx: int) -> list:
-        traverse = self.__traverseList(fromIdx + 1)
+        traverse = self.traverseList(fromIdx)
         elements = []
         while fromIdx <= toIdx:
             elements.append(traverse.data)
@@ -112,22 +112,79 @@ class SinglyLinkedList:
     def isEmpty(self) -> bool:
         return self.__length == 0
 
-
+                # =========================================================================================================== #
 
 class DoublyLinkedList(SinglyLinkedList):  # DoublyLinkedList inherits from SinglyLinkedList
     def __init__(self) -> None:
         super().__init__()
         self.prev = None
-
-
-
-
+        self.__length = 0
+        
+    def insertAt(self, newNode: DNode, index: int) -> None:
+        if index == self.__length:   # insert at the end
+            self.insertAtEnd(newNode)
+            return
+        elif index == 0:
+            self.insertAtHead(newNode)
+            return
+        
+        self.__length += 1
+        traverse = super().traverseList(index)
+        self.prevNode.next = newNode
+        newNode.prev = self.prevNode
+        newNode.next = traverse
+        traverse.prev = newNode
+        
+    def insertAtHead(self, newNode):
+        temp = self.head
+        temp.prev = newNode
+        newNode.next = temp
+        self.head = newNode
+        del temp
+        
+    def insertAtEnd(self, newNode: DNode) -> None:
+        self.__length += 1
+        if self.head is None:
+            self.head = newNode
+            return
+        
+        lastNode = self.head
+        while lastNode.next is not None:
+            lastNode = lastNode.next
+        lastNode.next = newNode
+        newNode.prev = lastNode
+        
+    def clear(self) -> None:
+        self.__length = 0
+        super().clear()
+        
+    @property
+    def Size(self) -> int:
+        return self.__length
+    
+    @property
+    def isEmpty(self) -> bool:
+        return self.__length == 0
+    
 
 
 Slinkedlist = SinglyLinkedList()  # creating a SinglyLinkedList instance
-numOfNodes = int(input("Enter the number of nodes: "))
-for i in range(numOfNodes):
-    node = SNode(input(f"Enter the data of node {i+1}: ")) # create a node. All the data are of type str
-    Slinkedlist.insertAtEnd(node) # insert the node
-print(Slinkedlist.isEmpty)
+Dlinkedlist = DoublyLinkedList()  # creating a DoublyLinkedList instance
+node1 = DNode("100")
+node2 = DNode("200")
+node3 = DNode("300")
+node4 = DNode("400")
+Dlinkedlist.insertAtEnd(node1)
+Dlinkedlist.insertAtEnd(node2)
+Dlinkedlist.insertAtEnd(node3)
+Dlinkedlist.insertAtEnd(node4)
+
+
+
+# numOfSNodes = int(input("Enter the number of nodes: "))
+# for i in range(numOfSNodes):
+#     node = SNode(input(f"Enter the data of node {i+1}: ")) # create a node. All the data are of type str
+#     Slinkedlist.insertAtEnd(node) # insert the node
+    
+# Slinkedlist.printLS()
 
